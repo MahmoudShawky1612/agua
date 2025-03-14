8e3ff6ffa3464736ab0cde515877335705325e0f
Dodje <nostyy30@gmail.com>
2025-03-15 01:01:36 +0200

    delete raw

--- src/routes/raw.route.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-import { getRaw } from '../controllers/raw.controller';
-
-router.get('/get-raw/:userId', getRaw);
-
-export default router;
